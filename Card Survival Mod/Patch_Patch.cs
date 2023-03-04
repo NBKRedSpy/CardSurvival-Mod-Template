326e943c3bc0a3b6f47abe117c1384e65fd7ce97
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-03-04 17:38:14 -0500

    init

---
+++ Card Survival Mod/Patch_Patch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace $safeprojectname$
+{
+    [HarmonyPatch(typeof(BlueprintModelsScreen), "ShowTab")]
+    public static class Patch_Patch
+    {
+        public static void Postfix(BlueprintModelsScreen __instance)
+        {
+        }
+
+    }
+}
